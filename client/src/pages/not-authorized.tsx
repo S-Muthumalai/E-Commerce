c72c36cc06a3699eb42b7ca49caddf47514a96e4
S-Muthumalai <167616476+S-Muthumalai@users.noreply.github.com>
2025-10-07 10:31:00 +0530

    Delete client directory

--- client/src/pages/not-authorized.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotAuthorized() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
-        <h1 className="text-2xl font-bold text-gray-900">You are not authorized to access this page.</h1>
-      </div>
-    );
-  }
